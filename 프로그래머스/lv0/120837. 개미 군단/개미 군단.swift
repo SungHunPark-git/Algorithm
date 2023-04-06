e701eeff3751ede53b12b11221538ada8919d441
ParkSungHun <114156413+SungHunPark-git@users.noreply.github.com>
2023-04-07 00:08:04 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.swift
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.swift
@@ -2,23 +2,12 @@ import Foundation
 
 func solution(_ hp:Int) -> Int {
     
-var numberOfAnt: Int = 0
 var hp = hp
+var result: Int = 0
     
-    if hp % 5 != 0 {
-    numberOfAnt += hp / 5
-    hp %= 5
-    if hp % 3 != 0 {
-        numberOfAnt += hp / 3
-        hp %= 3
-        numberOfAnt += hp / 1
-    } else {
-        numberOfAnt += hp / 3
+    for i in [5, 3, 1] {
+        result += hp / i
+        hp %= i
     }
-    
-} else {
-    numberOfAnt = hp / 5
-}
-    
-    return numberOfAnt
+    return result
 }
