22e343d0d6caccda951b4dc4f274ced06c5ad261
ParkSungHun <114156413+SungHunPark-git@users.noreply.github.com>
2023-04-11 14:50:57 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.swift
+++ 프로그래머스/lv0/120829. 각도기/각도기.swift
@@ -2,6 +2,6 @@ import Foundation
 
 func solution(_ angle:Int) -> Int {
     
-    return angle > 0 && angle < 90 ? 1 : angle == 90 ? 2 : angle > 90 && angle < 180 ? 3 : angle == 180 ? 4 : 0
+    return angle < 90 ? 1 : angle == 90 ? 2 : angle < 180 ? 3 : 4 
     
 }
